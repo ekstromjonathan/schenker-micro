483aa127b1d453691e2823003b00c090dc34ef4f
Jonathan Garvey <jonathan.garvey@pathwaysplatform.com>
2014-06-13 09:55:12 -0700

    Populate case studies with DB info, update read more functionality

--- assets/main.js
+++ assets/main.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
 	// full bio functionality
 	$('.readMore').on('click', function (e) {
 		e.preventDefault();
-		$(this).siblings().filter('.bio').slideDown('400', function() {
+		$(this).siblings().filter('.showMore').slideDown('400', function() {
 			$(this).siblings().filter('.readMore').hide();
 			$(this).siblings().filter('.readLess').show();
 		});
 	});
 	$('.readLess').on('click', function (e) {
 		e.preventDefault();
-		$(this).siblings().filter('.bio').slideUp('400', function() {
+		$(this).siblings().filter('.showMore').slideUp('400', function() {
 			$(this).siblings().filter('.readMore').show();
 			$(this).siblings().filter('.readLess').hide();
 		});
