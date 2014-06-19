82aa03d9db6ec2e8b9fa698fe38a3b1cbd7031c1
Jonathan Garvey <jonathan.garvey@pathwaysplatform.com>
2014-06-19 10:39:59 -0700

    Hook up contact form functionality

---
+++ contactengine.php
@@ -0,0 +1,40 @@
+<?php
+
+$EmailTo = "jon.d.garvey@gmail.com";
+$Subject = "Contact from DB Schenker Microsite";
+$Name = Trim(stripslashes($_POST['name'])); 
+$Email = Trim(stripslashes($_POST['email'])); 
+$Message = Trim(stripslashes($_POST['message'])); 
+$EmailFromName = $Name;
+$EmailFromEmail = $Email;
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: $EmailFromName <$EmailFromEmail>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.html\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
